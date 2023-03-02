214473862440271d04982540560d5f4a341ec20c
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-03-03 03:27:37 +0600

    modal left side done

--- js/ai.js
+++ js/ai.js
@@ -61,30 +61,48 @@ const showModalDetails = (id) => {
   const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
   fetch(URL)
     .then((res) => res.json())
-    .then((data) =>openModalDetails(data.data))
+    .then((data) => openModalDetails(data.data))
     .catch((err) => console.log(err));
 };
 const openModalDetails = (tools) => {
-  console.log(tools.features[1])
   const leftSide = document.getElementById("modal-body-left");
-  leftSide.innerHTML=`
+  leftSide.innerHTML = `
   
-  <h3>${tools.description? tools.description:"No Description"}</h3>
+  <h3>${tools.description ? tools.description : "No Description"}</h3>
   <div class="d-flex justify-content-between gap-2">
-      <div class="fs-3 fw-semibold bg-body-secondary text-success p-3">${tools.pricing[0].price} Basic</div>
-      <div class="fs-3 fw-semibold bg-body-secondary text-warning p-3">${tools.pricing[1].price} Pro</div>
-      <div class="fs-3 fw-semibold bg-body-secondary text-danger p-3">${tools.pricing[2].price}</div>
+      <div class="fs-3 fw-semibold bg-body-secondary text-success p-3">${
+        tools.pricing[0].price
+      } Basic</div>
+      <div class="fs-3 fw-semibold bg-body-secondary text-warning p-3">${
+        tools.pricing[1].price
+      } Pro</div>
+      <div class="fs-3 fw-semibold bg-body-secondary text-danger p-5">${
+        tools.pricing[2].price
+      }</div>
     </div>
-    <div class="d-flex justify-content-between gap-2">
-      <ul class="mx-2">
-        <li>${tools.features[1]? tools.features[1].feature_name: ""}</li>
-        <li>${tools.features[2]? tools.features[2].feature_name: ""}</li>
-        <li>${tools.features[3]? tools.features[3].feature_name: ""}</li>
-        <li>${tools.features[4]? tools.features[4].feature_name: ""}</li>
-      </ul>
-      <div></div>
+    <div class="d-flex justify-content-between gap-2 mt-4">
+      
+      <div class="mx-2">
+      <h3>Features</h3>
+          <ul>
+              <li>${tools.features[1] ? tools.features[1].feature_name : ""}</li>
+              <li>${tools.features[2] ? tools.features[2].feature_name : ""}</li>
+              <li>${tools.features[3] ? tools.features[3].feature_name : ""}</li>
+              <li>${tools.features[4] ? tools.features[4].feature_name : ""}</li>
+          </ul>
+      </div>
+        <div class="mx-2">
+        <h3>Integrations</h3>
+          <ul>
+            <li>${tools.integrations[0] ? tools.integrations[0] : ""}</li>
+            <li>${tools.integrations[1] ? tools.integrations[1] : ""}</li>
+            <li>${tools.integrations[2] ? tools.integrations[2] : ""}</li>
+            <li>${tools.integrations[3] ? tools.integrations[3] : ""}</li>
+            <li>${tools.integrations[4] ? tools.integrations[4] : ""}</li>
+          </ul>
+        </div>
     </div>
-  
-  `
-}
+  `;
 
+  console.log(tools);
+};
