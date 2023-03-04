7248ccab1592436c20f1d641ee8f7a86e6af20c6
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-03-04 13:32:09 +0600

    accurecy disply hide

--- js/ai.js
+++ js/ai.js
@@ -28,17 +28,21 @@ const displayAI = (tools) => {
     <div class="card-body">
       <h5 class=" fs-2">Features</h5>
       <ol class="card-text text-body-secondary">
-      ${tool.features[0] ? `<li>${tool.features[0]}</li>` : ''}
-      ${tool.features[1] ? `<li>${tool.features[1]}</li>` : ''}
-      ${tool.features[2] ? `<li>${tool.features[2]}</li>` : ''}
+      ${tool.features[0] ? `<li>${tool.features[0]}</li>` : ""}
+      ${tool.features[1] ? `<li>${tool.features[1]}</li>` : ""}
+      ${tool.features[2] ? `<li>${tool.features[2]}</li>` : ""}
       </ol>
     </div> <hr>
     <div class="d-flex justify-content-between align-items-center">
       <div>
         <h5 class="fs-2 card-title">${tool.name}</h5>
-        <p class="card-text text-body-secondary"><i class="fa-solid fa-calendar-days mx-2 fw-bold"></i>${tool.published_in}</p>
+        <p class="card-text text-body-secondary"><i class="fa-solid fa-calendar-days mx-2 fw-bold"></i>${
+          tool.published_in
+        }</p>
       </div>
-      <button onclick="showModalDetails('${tool.id}')" class="btn fs-3"  data-bs-toggle="modal" data-bs-target="#AIDetailsModal">
+      <button onclick="showModalDetails('${
+        tool.id
+      }')" class="btn fs-3"  data-bs-toggle="modal" data-bs-target="#AIDetailsModal">
         <i class="fa-solid fa-arrow-right border p-2 bg-danger bg-opacity-10 rounded-circle"></i>
       </button>
     </div>
@@ -47,8 +51,8 @@ const displayAI = (tools) => {
     `;
       showUIDetails.appendChild(show);
     });
-    // stop spinner 
-document.getElementById("spinner").classList.add("d-none");
+    // stop spinner
+    document.getElementById("spinner").classList.add("d-none");
     if (toolsToDisplay.length === tools.length) {
       showAll.classList.add("d-none");
     } else {
@@ -69,49 +73,36 @@ const showModalDetails = (id) => {
     .then((data) => openModalDetails(data.data))
     .catch((err) => console.log(err));
 };
+
 const openModalDetails = (tools) => {
   document.getElementById("modal-body-right").innerHTML= "";
   const leftSide = document.getElementById("modal-body-left");
   leftSide.innerHTML = `
-  
-  <h3>${tools.description ? tools.description : "No Description"}</h3>
-  <div class="d-md-flex justify-content-between gap-2">
-      <div class="fs-3 fw-semibold bg-body-secondary text-success p-1">${
-        tools.pricing !==null ? tools.pricing[0].price : ""
-      } Basic</div>
-      <div class="fs-3 fw-semibold bg-body-secondary text-warning p-1">${
-        tools.pricing !==null ? tools.pricing[1].price : ""
-      } Pro</div>
-      <div class="fs-3 fw-semibold bg-body-secondary text-danger p-2">${
-        tools.pricing !==null ? tools.pricing[2].price : ""}</div>
+    <h3>${tools.description ? tools.description : "No Description"}</h3>
+    <div class="d-md-flex justify-content-between gap-2">
+      <div class="fs-3 fw-semibold bg-body-secondary text-success p-1">${tools.pricing !== null ? tools.pricing[0].price : ""} ${tools.pricing !== null ? tools.pricing[0].plan : ""}</div>
+      <div class="fs-3 fw-semibold bg-body-secondary text-warning p-1">${tools.pricing !== null ? tools.pricing[1].price : ""} ${tools.pricing !== null ? tools.pricing[1].plan : ""}</div>
+      <div class="fs-3 fw-semibold bg-body-secondary text-danger p-2">${tools.pricing !== null ? tools.pricing[2].price : ""}</div>
     </div>
   `;
+  
   // leftSide modal feature 
-let tool = Object.entries(tools.features); 
-document.getElementById("left-features").innerHTML ="";
-tool.forEach(function (item) {
-  // console.log(item[1].feature_name);
-  document.getElementById("left-features").innerHTML += `<li>${item[1].feature_name}</li>`
-});
+  let tool = Object.entries(tools.features); 
+  document.getElementById("left-features").innerHTML ="";
+  tool.forEach((item) => document.getElementById("left-features").innerHTML += `<li>${item[1].feature_name}</li>`);
 
-// rightSide modal Feature
-document.getElementById("right-features").innerHTML="";
-let value= tools.integrations;
-value.forEach(function (item) {
-  console.log(item);
-  document.getElementById("right-features").innerHTML += `<li>${item !==null? item : "No data Found"}</li>`
-});
+  // rightSide modal Feature
+  document.getElementById("right-features").innerHTML="";
+  let value = tools.integrations;
+  value.forEach((item) => document.getElementById("right-features").innerHTML += `<li>${item !== null ? item : "No data Found"}</li>`);
 
-
-console.log(tools);
-const rightSide= document.getElementById("modal-body-right");
+  const rightSide = document.getElementById("modal-body-right");
   rightSide.innerHTML=`
-           <img class="img-fluid" src="${tools.image_link !==null?tools.image_link[0] : ""}" alt="">
-           <p class="fs-3 m-4 position-absolute top-0 end-0 badge border border-light  bg-danger p-2">${(tools.accuracy !==0? tools.accuracy.score*100 : "" )}% accuracy</p>
-           <h3 class="text-center my-3 fw-bold">${tools.input_output_examples !==null? tools.input_output_examples[0].input : ""}</h3>
-           <p class="text-center">${tools.input_output_examples[0].output}</p>
+    <img class="img-fluid" src="${tools.image_link !== null ? tools.image_link[0] : ""}" alt="">
+    <p class="fs-3 m-4 position-absolute top-0 end-0 badge border border-light  bg-danger p-2">${tools.accuracy.score !== null ? tools.accuracy.score * 100 +"% accuracy"  : ""}</p>
+    <h3 class="text-center my-3 fw-bold">${tools.input_output_examples !== null ? tools.input_output_examples[0].input : ""}</h3>
+    <p class="text-center">${tools.input_output_examples !== null ? tools.input_output_examples[0].output : ""}</p>
   `;
-  // console.log(tools.features);
 };
 
 const sortByDate = () => {
@@ -119,7 +110,9 @@ const sortByDate = () => {
   fetch(URL)
     .then((res) => res.json())
     .then((data) => {
-      const sortedTools = data.data.tools.sort((a, b) => new Date(a.published_in) - new Date(b.published_in));
+      const sortedTools = data.data.tools.sort(
+        (a, b) => new Date(a.published_in) - new Date(b.published_in)
+      );
       displayAI(sortedTools);
     })
     .catch((err) => console.log(err));
